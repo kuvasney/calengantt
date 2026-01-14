409bbcff0f4fe53151905284d0df035412f03bf2
kuvasney <kuvasney@gmail.com>
2026-01-14 00:39:13 -0300

    refactor: comment out fetchCommentsForProject implementation in useCommentsApi

--- src/hooks/useCommentsApi.ts
+++ src/hooks/useCommentsApi.ts
@@ -1,29 +1,27 @@
-import { API_CONFIG, API_ENDPOINTS } from "@/config/api";
-import type { Comments } from "@/types/project";
-import { useCallback } from "react";
+// import { API_CONFIG, API_ENDPOINTS } from "@/config/api";
+// import type { Comments } from "@/types/project";
+// import { useCallback } from "react";
 
 export const useCommentsApi = () => {
-  const fetchCommentsForProject = useCallback(
-    async (projectId: string): Promise<Comments[]> => {
-      const response = await fetch(
-        `${API_CONFIG.baseURL}${API_ENDPOINTS.project}/${projectId}/comments`,
-        {
-          method: "GET",
-          headers: API_CONFIG.headers,
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(
-          `Erro ao buscar os comentários: ${response.statusText}`
-        );
-      }
-
-      const data: Comments[] = await response.json();
-      return data;
-    },
-    []
-  );
-
-  return { fetchCommentsForProject };
+  // TO BE IMPLEMENTED
+  // const fetchCommentsForProject = useCallback(
+  //   async (projectId: string): Promise<Comments[]> => {
+  //     const response = await fetch(
+  //       `${API_CONFIG.baseURL}${API_ENDPOINTS.project}/${projectId}/comments`,
+  //       {
+  //         method: "GET",
+  //         headers: API_CONFIG.headers,
+  //       }
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error(
+  //         `Erro ao buscar os comentários: ${response.statusText}`
+  //       );
+  //     }
+  //     const data: Comments[] = await response.json();
+  //     return data;
+  //   },
+  //   []
+  // );
+  // return { fetchCommentsForProject };
 };
